16a4a6824b3cbbad937b44dbe183d5d5bfb26a02
Vincenzo Tardino #66 <101521125+Zibo88@users.noreply.github.com>
2023-01-24 17:53:34 +0100

    Add files via upload

---
+++ src/org/lesson/bonus/java/myBonus.java
@@ -0,0 +1,7 @@
+package org.lesson.bonus.java;
+
+public class myBonus {
+	public static void main(String[] args ) {
+		System.out.print("secondo ciao");
+	}
+}
---
+++ src/org/lesson/java/stampaSaluto.java
@@ -0,0 +1,7 @@
+package org.lesson.java;
+
+public class stampaSaluto {
+	public static void main(String[] args) {
+		System.out.print("ciao");
+	}
+}
